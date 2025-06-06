5ffa6a8cdde56d2643199aac51b9d3ec161af350
Wes Shaw <101996321+wes-shaw@users.noreply.github.com>
2025-06-06 23:38:02 +0000

    shot radius

--- player.py
+++ player.py
@@ -43,5 +43,5 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
